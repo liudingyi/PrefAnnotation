667eb180887a7546b05dd279112fdbae69403764
liudingyi <1013731419@qq.com>
2018-06-14 18:03:23 +0800

    配置本地仓库

--- app/src/main/java/com/pref/annotation/MainActivity.java
+++ app/src/main/java/com/pref/annotation/MainActivity.java
@@ -3,11 +3,14 @@ package com.pref.annotation;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pref.Pref;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Pref pref = new Pref(this);
     }
 }
